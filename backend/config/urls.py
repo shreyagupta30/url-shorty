1c4604cd7c18469da30217391e6682afd4b32440
shreyagupta30 <shreyagupta3011@gmail.com>
2020-08-17 17:53:52 +0530

    configure backend url

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,7 +19,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 from graphene_django.views import GraphQLView
 
-from shortner.views import root
+from backend.shortner.views import root
 
 urlpatterns = [
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
