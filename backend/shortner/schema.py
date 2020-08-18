0fc59b98b8a304727da58f04c3a9ac2ec8cc039c
shreyagupta30 <shreyagupta3011@gmail.com>
2020-08-18 20:23:26 +0530

    change in shortner/schema.py

--- backend/shortner/schema.py
+++ backend/shortner/schema.py
@@ -3,15 +3,18 @@ from graphene_django import DjangoObjectType
 
 from .models import URL
 
+
 class URLType(DjangoObjectType):
     class Meta:
         model = URL
 
-class Query(graphene.ObjectType):
-    urls = graphene.List(URLType, url=graphene.String())
 
-    def resolve_urls(self, info, url=None, **kwargs):
-        queryset = URL.objects.all()
+class Query(graphene.ObjectType):
+    urls = graphene.List(URLType)
+
+    def resolve_urls(self, info, **kwargs):
+        return URL.objects.all()
+
 
 class CreateURL(graphene.Mutation):
     url = graphene.Field(URLType)
@@ -24,6 +27,7 @@ class CreateURL(graphene.Mutation):
         url.save()
 
         return CreateURL(url=url)
+
 
 class Mutation(graphene.ObjectType):
     create_url = CreateURL.Field()
