7fb4ae409cbae09730e1fcfa3df6e00f2128a61c
shreyagupta30 <shreyagupta3011@gmail.com>
2020-08-21 23:55:49 +0530

    modify query.js

--- src/components/query/Query.js
+++ src/components/query/Query.js
@@ -2,38 +2,44 @@ import React from 'react';
 import { useQuery } from 'react-apollo';
 import { gql } from 'apollo-boost';
 
-const QUERY_URL = gql`
-query {
-    urls {
-      id
-      fullUrl
-      urlHash
-      clicks
-      createdAt
-    }
-}`;
+class fullUrl extends React.Component {
+  
+  QUERY_URL = gql`
+  query {
+      urls {
+        id
+        fullUrl
+        urlHash
+        clicks
+        createdAt
+      }
+  }`;
+  
 
-const fullURL=() =>{
-  
-  const { data, loading } = useQuery(
-    QUERY_URL, {
-      pollInterval: 500 
+  render(
+  fullURL= (prop) =>{
+    const { data, loading } = useQuery(
+      this.QUERY_URL, {
+        pollInterval: 500 
+      }
+    );
+    
+    if (loading) return <p> Loading... </p>;
+    <>
+    return data.users.map(({ id, fullUrl, urlHash, createdAt, clicks }) => (
+      <div key={id}>
+        <p>
+          History - {id}
+          Full Url = {fullUrl}
+          Shortened url = http://localhost:8000/{urlHash}
+          Created at: {createdAt} 
+          Number of clicks: {clicks}
+        </p>
+      </div>
+    ));
+    </>
     }
-  );
-  
-  if (loading) return <p> Loading... </p>;
-   
-  return data.users.map(({ id, fullUrl, urlHash, createdAt, clicks }) => (
-    <div key={id}>
-      <p>
-        History - {id}
-        Full Url = {fullUrl}
-        Shortened url = http://localhost:8000/{urlHash}
-        Created at: {createdAt} 
-        Number of clicks: {clicks}
-      </p>
-    </div>
-  ));
-}
+  )
+};
 
 export default fullURL;
